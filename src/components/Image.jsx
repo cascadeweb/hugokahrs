4476e0f94ae301f39d2884bbefe6cf6440f54100
cascade <victortestnet@gmail.com>
2023-12-28 13:44:46 +0100

    change

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,7 @@
 const images = [
   { url: "/Art1.png", alt: "Image 1" },
   { url: "/Art2.png", alt: "Image 2" },
-  { url: "/Art1.png", alt: "Image 3" },
+  { url: "/Art3.png", alt: "Image 3" },
 ];
 
 const Image = () => {
@@ -9,11 +9,11 @@ const Image = () => {
     <section className="flex-1 flex flex-col items-center bg-gray-100 p-4">
       <div className="flex flex-wrap justify-center">
         {images.map((image, index) => (
-          <div key={index} className="max-w-md mb-4 mx-2">
+          <div key={index} className="w-64 h-64 mx-2 mb-4 overflow-hidden">
             <img
               src={image.url}
               alt={image.alt}
-              className="w-full h-64"
+              className="w-full h-full object-none"
             />
           </div>
         ))}
